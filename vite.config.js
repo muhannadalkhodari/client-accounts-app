74e0c1412810dd4ccf5dfcae2745d2675185e01e
Muhannad Al-Khodari <60114338+muhannadalkhodari@users.noreply.github.com>
2026-09-21 14:37:40 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
+      includeAssets: ['favicon.ico', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-512-maskable.png'],
       manifest: {
         name: 'حسابات العملاء',
         short_name: 'حسابات العملاء',
